65bf117a067b0c135b0cb4235305b12634789010
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 00:18:27 -0300

    feat: add atualizar estoque controller

---
+++ src/presentation/controllers/atualizar-estoque.ts
@@ -0,0 +1,20 @@
+import { AtualizarEstoqueUseCase } from "../../domain/useCases/atualizar-estoque"
+import { Validator } from "../../validation/contracts/validator"
+import { Controller } from "../contracts/controller"
+import { AMQPRequest } from "../contracts/amqp";
+
+
+export class AtualizarEstoqueController implements Controller {
+    constructor(private readonly validator: Validator, private readonly atualizarEstoqueUseCase: AtualizarEstoqueUseCase) { }
+    async handle(AMQPRequest: AMQPRequest): Promise<any> {
+        const error = this.validator.validate(AMQPRequest.payload)
+        if (error) {
+            throw error
+        }
+        const { nome, id } = AMQPRequest.payload
+        await this.atualizarEstoqueUseCase.atualizar({
+            id,
+            nome
+        })
+    }
+}
