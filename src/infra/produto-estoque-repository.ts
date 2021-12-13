f6dafea4892b7d70cd0eff2e3bde494fbbec1d45
“Breno <brenorodriguesdev@gmail.com>
2021-12-12 23:32:04 -0300

    feat: implements produto estoque repository

---
+++ src/infra/produto-estoque-repository.ts
@@ -0,0 +1,24 @@
+import { getRepository } from "typeorm"
+import { ProdutoEstoqueRepository } from "../data/contracts/produtoEstoque-repository"
+import { ProdutoEstoque } from "../data/entities/produtoEstoque"
+
+export class ProdutoEstoqueRepositoryTypeORM implements ProdutoEstoqueRepository {
+
+    async findByEstoque(idEstoque: number): Promise<ProdutoEstoque[]> {
+        const produtoEstoqueRepository = getRepository(ProdutoEstoque)
+        return await produtoEstoqueRepository.find({ idEstoque })
+    }
+
+    async create(produtoEstoque: ProdutoEstoque): Promise<void> {
+        const produtoEstoqueRepository = getRepository(ProdutoEstoque)
+        produtoEstoque.id = await produtoEstoqueRepository.count() + 1
+
+        await produtoEstoqueRepository.save(produtoEstoque)
+    }
+
+    async delete(produtoEstoque: ProdutoEstoque): Promise<void> {
+        const produtoEstoqueRepository = getRepository(ProdutoEstoque)
+        await produtoEstoqueRepository.delete(produtoEstoque)
+    }
+
+}
