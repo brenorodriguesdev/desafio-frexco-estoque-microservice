ce619128c845e5d38372297841aae4ebe041aec6
“Breno <brenorodriguesdev@gmail.com>
2021-12-13 04:39:45 -0300

    feat: add buscar estoque queue

--- src/main/queues/estoque.ts
+++ src/main/queues/estoque.ts
@@ -1,5 +1,6 @@
 import { adaptQueue } from "../adapters/amqp-controller";
 import { makeAtualizarEstoqueController } from "../factories/controllers/atualizar-estoque";
+import { makeBuscarEstoqueController } from "../factories/controllers/buscar-estoque";
 import { makeCriarEstoqueController } from "../factories/controllers/criar-estoque";
 import { makeDeletarEstoqueController } from "../factories/controllers/deletar-estoque";
 import { makeListarEstoquesController } from "../factories/controllers/listar-estoques";
@@ -9,4 +10,5 @@ export default (channel: any): void => {
     channel.consume('atualizar-estoque', adaptQueue(channel, makeAtualizarEstoqueController()));
     channel.consume('deletar-estoque', adaptQueue(channel, makeDeletarEstoqueController()));
     channel.consume('listar-estoques', adaptQueue(channel, makeListarEstoquesController()));
+    channel.consume('buscar-estoque', adaptQueue(channel, makeBuscarEstoqueController()));
 }
