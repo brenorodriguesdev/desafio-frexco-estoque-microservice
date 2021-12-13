15076bfe30dc7369466a40ebde18f7abbec0f06c
“Breno <brenorodriguesdev@gmail.com>
2021-12-12 23:15:02 -0300

    feat: add listar produto estoque por estoque service

---
+++ src/data/services/listar-produto-estoque-por-estoque.ts
@@ -0,0 +1,11 @@
+import { ProdutoEstoqueModel } from "../../domain/models/produtoEstoque"
+import { ListarProdutoEstoquePorEstoqueUseCase } from "../../domain/useCases/listar-produto-estoque-por-estoque"
+import { ProdutoEstoqueRepository } from "../contracts/produtoEstoque-repository"
+
+export class ListarProdutoEstoquePorEstoqueService implements ListarProdutoEstoquePorEstoqueUseCase {
+    constructor (private readonly produtoEstoqueRepository: ProdutoEstoqueRepository) {}
+    async listar (idEstoque: number): Promise<ProdutoEstoqueModel[]> {
+        await this.produtoEstoqueRepository.findByEstoque(idEstoque)
+        return []
+    }
+}
