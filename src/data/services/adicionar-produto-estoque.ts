7a8f111c7f72db517094d3abc85beeb23cc2b683
“Breno <brenorodriguesdev@gmail.com>
2021-12-15 03:22:01 -0300

    feat: add adicionar produto estoque service

--- src/data/services/adicionar-produto-estoque.ts
+++ src/data/services/adicionar-produto-estoque.ts
@@ -1,10 +1,19 @@
 import { AdicionarProdutoEstoqueModel } from "../../domain/models/adicionar-produto-estoque"
 import { AdicionarProdutoEstoqueUseCase } from "../../domain/useCases/adicionar-produto-estoque"
 import { ProdutoEstoqueRepository } from "../contracts/produtoEstoque-repository"
+import { Estoque } from "../entities/estoque"
+import { Produto } from "../entities/produto"
 
 export class AdicionarProdutoEstoqueService implements AdicionarProdutoEstoqueUseCase {
     constructor (private readonly produtoEstoqueRepository: ProdutoEstoqueRepository) {}
     async adicionar (data: AdicionarProdutoEstoqueModel): Promise<void | Error> {
-        await this.produtoEstoqueRepository.create(data)
+        const produto = new Produto()
+        produto.id = data.idProduto
+        const estoque = new Estoque()
+        estoque.id = data.idEstoque
+        await this.produtoEstoqueRepository.create({
+            produto,
+            estoque
+        })
     }
 }
