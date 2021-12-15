d2c134aa20df251d59bafd65b69b9c1fa6d9439d
“Breno <brenorodriguesdev@gmail.com>
2021-12-15 03:22:52 -0300

    feat: add produto estoque queue

--- src/main/queues/produtoEstoque.ts
+++ src/main/queues/produtoEstoque.ts
@@ -1,9 +1,10 @@
 import { adaptQueue } from "../adapters/amqp-controller";
 import { makeAdicionarProdutoEstoqueController } from "../factories/controllers/adicionar-produto-estoque";
 import { makeDeletarProdutoEstoqueController } from "../factories/controllers/deletar-produto-estoque";
+import { makeListarProdutoEstoquePorEstoqueController } from "../factories/controllers/listar-produto-estoque-por-estoque";
 
 export default (channel: any): void => {
     channel.consume('adicionar-produtoEstoque', adaptQueue(channel, makeAdicionarProdutoEstoqueController()));
     channel.consume('deletar-produtoEstoque', adaptQueue(channel, makeDeletarProdutoEstoqueController()));
-    channel.consume('listar-produtoEstoquePorEstoque', adaptQueue(channel, makeDeletarProdutoEstoqueController()));
+    channel.consume('listar-produtoEstoquePorEstoque', adaptQueue(channel, makeListarProdutoEstoquePorEstoqueController()));
 }
