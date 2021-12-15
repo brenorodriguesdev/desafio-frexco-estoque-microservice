1c97752508cd0775375cada35996cbaa6b226114
“Breno <brenorodriguesdev@gmail.com>
2021-12-15 03:22:10 -0300

    feat: add deletar produto estoque service

--- src/data/services/deletar-produto-estoque.ts
+++ src/data/services/deletar-produto-estoque.ts
@@ -1,10 +1,19 @@
 import { AdicionarProdutoEstoqueModel } from "../../domain/models/adicionar-produto-estoque"
 import { DeletarProdutoEstoqueUseCase } from "../../domain/useCases/deletar-produto-estoque"
 import { ProdutoEstoqueRepository } from "../contracts/produtoEstoque-repository"
+import { Estoque } from "../entities/estoque"
+import { Produto } from "../entities/produto"
 
 export class DeletarProdutoEstoqueService implements DeletarProdutoEstoqueUseCase {
     constructor (private readonly produtoEstoqueRepository: ProdutoEstoqueRepository) {}
     async deletar (data: AdicionarProdutoEstoqueModel): Promise<void | Error> {
-        await this.produtoEstoqueRepository.delete(data)
+        const produto = new Produto()
+        produto.id = data.idProduto
+        const estoque = new Estoque()
+        estoque.id = data.idEstoque
+        await this.produtoEstoqueRepository.delete({
+            produto,
+            estoque
+        })
     }
 }
