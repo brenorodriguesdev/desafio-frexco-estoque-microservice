9c18bd8be3565abbcd2038cea19911e8322c549f
“Breno <brenorodriguesdev@gmail.com>
2021-12-16 03:15:59 -0300

    fix: factories

--- src/main/factories/controllers/adicionar-produto-estoque.ts
+++ src/main/factories/controllers/adicionar-produto-estoque.ts
@@ -1,10 +1,14 @@
 import { AdicionarProdutoEstoqueService } from "../../../data/services/adicionar-produto-estoque"
+import { AMQPClientAdapter } from "../../../infra/amqp-client"
+import { EstoqueRepositoryTypeORM } from "../../../infra/estoque-repository"
 import { ProdutoEstoqueRepositoryTypeORM } from "../../../infra/produto-estoque-repository"
 import { AdicionarProdutoEstoqueController } from "../../../presentation/controllers/adicionar-produto-estoque"
 import { makeAdicionarProdutoEstoqueValidator } from "../validators/adicionar-produto-estoque"
 
 export const makeAdicionarProdutoEstoqueController = (): AdicionarProdutoEstoqueController => {
+    const amqpClientAdapter = new AMQPClientAdapter()
+    const estoqueRepositoryTypeORM = new EstoqueRepositoryTypeORM()
     const produtoEstoqueRepositoryTypeORM = new ProdutoEstoqueRepositoryTypeORM()
-    const adicionarProdutoEstoqueService = new AdicionarProdutoEstoqueService(produtoEstoqueRepositoryTypeORM)
+    const adicionarProdutoEstoqueService = new AdicionarProdutoEstoqueService(amqpClientAdapter, estoqueRepositoryTypeORM, produtoEstoqueRepositoryTypeORM)
     return new AdicionarProdutoEstoqueController(makeAdicionarProdutoEstoqueValidator(), adicionarProdutoEstoqueService)
 }
--- src/main/factories/controllers/deletar-produto-estoque.ts
+++ src/main/factories/controllers/deletar-produto-estoque.ts
@@ -1,10 +1,14 @@
 import { DeletarProdutoEstoqueService } from "../../../data/services/deletar-produto-estoque"
+import { AMQPClientAdapter } from "../../../infra/amqp-client"
+import { EstoqueRepositoryTypeORM } from "../../../infra/estoque-repository"
 import { ProdutoEstoqueRepositoryTypeORM } from "../../../infra/produto-estoque-repository"
 import { DeletarProdutoEstoqueController } from "../../../presentation/controllers/deletar-produto-estoque"
 import { makeDeletarProdutoEstoqueValidator } from "../validators/deletar-produto-estoque"
 
 export const makeDeletarProdutoEstoqueController = (): DeletarProdutoEstoqueController => {
+    const amqpClientAdapter = new AMQPClientAdapter()
+    const estoqueRepositoryTypeORM = new EstoqueRepositoryTypeORM()
     const produtoEstoqueRepositoryTypeORM = new ProdutoEstoqueRepositoryTypeORM()
-    const deletarProdutoEstoqueService = new DeletarProdutoEstoqueService(produtoEstoqueRepositoryTypeORM)
+    const deletarProdutoEstoqueService = new DeletarProdutoEstoqueService(amqpClientAdapter, estoqueRepositoryTypeORM, produtoEstoqueRepositoryTypeORM)
     return new DeletarProdutoEstoqueController(makeDeletarProdutoEstoqueValidator(), deletarProdutoEstoqueService)
 }
