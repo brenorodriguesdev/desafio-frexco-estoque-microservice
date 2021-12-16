23bf685fb2c98f1d0afc152836fc1a6b9b2a92eb
“Breno <brenorodriguesdev@gmail.com>
2021-12-16 05:28:49 -0300

    test: listar produto estoque por estoque controller

---
+++ src/presentation/controllers/listar-produto-estoque-por-estoque.spec.ts
@@ -0,0 +1,73 @@
+import { ProdutoEstoqueModel } from "../../domain/models/produtoEstoque"
+import { ListarProdutoEstoquePorEstoqueUseCase } from "../../domain/useCases/listar-produto-estoque-por-estoque"
+import { makeEstoque } from "../../tests/factories/entities/estoque"
+import { makeProduto } from "../../tests/factories/entities/produto"
+import { Validator } from "../../validation/contracts/validator"
+import { ListarProdutoEstoquePorEstoqueController } from "./listar-produto-estoque-por-estoque"
+
+interface SutTypes {
+    validator: Validator
+    listarProdutoEstoquePorEstoqueUseCase: ListarProdutoEstoquePorEstoqueUseCase,
+    sut: ListarProdutoEstoquePorEstoqueController
+}
+
+const makeProdutosEstoqueModel = (): ProdutoEstoqueModel[] => (
+    [{
+        id: 1,
+        produto: makeProduto(1),
+        estoque: makeEstoque(1)
+    }]
+)
+
+const makeListarProdutoEstoquePorEstoqueUseCase = (): ListarProdutoEstoquePorEstoqueUseCase => {
+    class ListarEstoquesUseCaseStub implements ListarProdutoEstoquePorEstoqueUseCase {
+        async listar(): Promise<ProdutoEstoqueModel[]> {
+            return new Promise(resolve => resolve(makeProdutosEstoqueModel()))
+        }
+    }
+    return new ListarEstoquesUseCaseStub()
+}
+
+const makeValidator = (): Validator => {
+    class ValidatorStub implements Validator {
+        validate(): Error {
+            return null
+        }
+    }
+    return new ValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+    const validator = makeValidator()
+    const listarProdutoEstoquePorEstoqueUseCase = makeListarProdutoEstoquePorEstoqueUseCase()
+    const sut = new ListarProdutoEstoquePorEstoqueController(validator, listarProdutoEstoquePorEstoqueUseCase)
+    return {
+        validator,
+        listarProdutoEstoquePorEstoqueUseCase,
+        sut
+    }
+}
+
+describe('listarProdutoEstoquePorEstoque controller', () => {
+
+    test('Garantir que listar seja chamado com os valores corretos', async () => {
+        const { sut, listarProdutoEstoquePorEstoqueUseCase } = makeSut()
+        const listarSpy = jest.spyOn(listarProdutoEstoquePorEstoqueUseCase, 'listar')
+        await sut.handle({ payload: { idEstoque: 1 } })
+        expect(listarSpy).toHaveBeenCalledWith(1)
+    })
+
+    test('Garantir que se o listar retornar uma exceção repassará essa exceção', async () => {
+        const { sut, listarProdutoEstoquePorEstoqueUseCase } = makeSut()
+        jest.spyOn(listarProdutoEstoquePorEstoqueUseCase, 'listar').mockImplementationOnce(() => { throw new Error() })
+        const promise = sut.handle({ payload: { idEstoque: 1 } })
+        await expect(promise).rejects.toThrow()
+    })
+
+    test('Garantir que se tudo ocorrer normalmente retornar produtos estoque', async () => {
+        const { sut } = makeSut()
+        const produtosEstoque = await sut.handle({ payload: { idEstoque: 1 } })
+        expect(produtosEstoque).toEqual(makeProdutosEstoqueModel())
+    })
+
+})
